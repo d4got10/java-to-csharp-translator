d731ce0a84fa3a3807d4b26876b9f1ea3dac07c2
Изотов Данил <rusbeks@outlook.com>
2022-09-29 15:15:27 +1000

    Create entry point for lexer

--- Lexer/Class1.cs
+++ Lexer/Class1.cs
@@ -1,5 +1,14 @@
 ﻿namespace Lexer;
 
-public class Class1
+public class Lexer
 {
+    /// <summary>
+    /// Parse specified file for a collection of tokens
+    /// </summary>
+    /// <param name="path">Path to text file</param>
+    /// <returns>Collection of tokens(strings)</returns>
+    public IEnumerable<string> Parse(string path)
+    {
+        return Enumerable.Empty<string>();
+    }
 }
