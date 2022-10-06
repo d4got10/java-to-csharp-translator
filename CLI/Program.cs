3cd994fb624c32a7cfb246c8203353adfa7b4c1b
Danil Izotov <rusbeks@outlook.com>
2022-10-06 14:39:51 +1000

    Add solution and projects to it.

---
+++ CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using LexicalAnalysis;
+
+const string path = "input.txt";
+
+if (!File.Exists(path))
+{
+    Console.WriteLine("File not found");
+    Console.WriteLine("Files present in the directory: ");
+    foreach(var fileName in Directory.GetFiles(AppContext.BaseDirectory))
+        Console.WriteLine(fileName);
+    return;
+}
+
+var lexemes = Lexer.Parse(ReadFile(path));
+foreach(var lexeme in lexemes)
+    Console.WriteLine(lexeme);
+
+
+static string ReadFile(string path) => File.ReadAllText(path);
---
+++ Lexer/Lexer.cs
@@ -0,0 +1,36 @@
+﻿namespace LexicalAnalysis
+{
+    public static class Lexer
+    {
+        /// <summary>
+        /// Parse specified text for a collection of tokens
+        /// </summary>
+        /// <param name="text">Text to parse</param>
+        /// <returns>Collection of tokens(strings)</returns>
+        public static IEnumerable<string> Parse(string text)
+        {
+            var words = text.Split(" ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Trim();
+                if (!ReservedWords.Contains(words[i]))
+                {
+                    words[i] = "ID " + words[i];
+                }
+            }
+            return words;
+        }
+
+        private static readonly HashSet<string> ReservedWords = new()
+        {
+            "for",
+            "int",
+            "(",
+            ")",
+            "=",
+            "<",
+            "++",
+            ";"
+        };
+    }
+}
